291f76a13d692b21aba9c46f72ca8152555c68c0
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-08-10 00:06:40 -0400

    debuging frontend

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
 
 // Helper function to read and write to db.json
